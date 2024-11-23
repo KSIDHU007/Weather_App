21b9c848f538b6ce3041d247d017fe6b92ce7cc7
KSIDHU007 <144937957+KSIDHU007@users.noreply.github.com>
2024-11-22 18:16:30 -0700

    Improve error handling and polish app design

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+document.getElementById('get-weather-button').addEventListener('click', () => {
+    const cityInput = document.getElementById('city-input').value.trim();
+    if (cityInput) {
+        fetchWeather(cityInput);
+    }
+});
+
 function fetchWeather(city) {
     const apiKey = '10099929769836d7022086023838b869';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -18,24 +25,34 @@ function fetchWeather(city) {
                 <p>Wind Speed: ${data.wind.speed} m/s</p>
             `;
 
-            // Dynamic Background
+            // Change background based on weather condition
             const condition = data.weather[0].main.toLowerCase();
             const body = document.body;
             switch (condition) {
                 case 'clear':
-                    body.style.backgroundImage = "url('./clear.gif')";
+                    body.style.backgroundImage = "url('./sun-7161738_1280.webp')";
                     break;
                 case 'clouds':
-                    body.style.backgroundImage = "url('./clouds.gif')";
+                    body.style.backgroundImage = "url('./ai-generated-8975560_1280.webp')";
                     break;
                 case 'rain':
-                    body.style.backgroundImage = "url('./rain.gif')";
+                    body.style.backgroundImage = "url('./ai-generated-8244285_960_720.png')";
                     break;
                 case 'snow':
-                    body.style.backgroundImage = "url('./snow.gif')";
+                case'light snow':
+                    body.style.backgroundImage = "url('./WhatsApp Image 2024-11-22 at 3.03.31 PM.jpeg')";
+                    break;
+                case 'mist':
+                case 'fog':
+                case 'smoke':
+                case 'haze':
+                    body.style.backgroundImage = "url('./autumn-1839969_1280.jpg')";
+                    break;
+                case 'thunderstorm':
+                    body.style.backgroundImage = "url('./thunderstorm-5680651_1280.jpg')";
                     break;
                 default:
-                    body.style.backgroundImage = "url('./default.gif')";
+                    body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?weather')";
             }
 
             body.style.backgroundSize = "cover";
